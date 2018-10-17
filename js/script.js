727ae381161645d3645e1a34fb62a31f65c9685a
Chris Hanson <BlondyHanson@gmail.com>
2018-10-17 10:29:53 +0100

    Final version

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ $('.main-carousel').flickity({
     contain: true
   });
 
+  $('#updates').on('submit', 'form', function(event) {
+    event.preventDefault();
+    let emailInput = $('#subscribe-email');
+    if(emailInput.val().length!==0) {
+        alert('You have been subscribed!');
+    } else {
+        alert('Please enter more information to be subscribed!');
+    }
+});
 
 });
 
