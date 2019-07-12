86708fbe88ab9ca9f6b6665ab3b42b45e1d2a806
Chris Hanson <BlondyHanson@gmail.com>
2019-07-12 21:11:56 +0100

    Clean up, amended subscribe email and fixed missing fonts

--- src/script.js
+++ src/script.js
@@ -1,6 +1,23 @@
 $('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true,
-  groupCells: true,
+	// options
+	cellAlign: 'left',
+	contain: true,
+	groupCells: true,
+});
+
+$('form').submit(function() {
+	var error;
+
+	if (!$('input').val()) {
+		error = true;
+  }
+  
+  if (error) {
+		alert('Please enter a valid email address!');
+		return false;
+  }
+  
+  alert('You have successfully been subscribed!');
+  event.preventDefault();
+  $('form').fadeOut(500);
 });
