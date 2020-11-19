ac195bd60c0299587fa1d3d2c6c9afc0543652df
Chris Hanson <BlondyHanson@gmail.com>
2020-11-19 15:56:53 +0000

    Upgraded eslint, added prettier, stylelint, husky & lint-staged to enforce code style

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-import Flickity from 'flickity';
-import 'flickity/css/flickity.css';
+import Flickity from 'flickity'
+import 'flickity/css/flickity.css'
 
 const carousel = new Flickity('.main-carousel', {
-  cellAlign: 'left',
-  contain: true,
-  groupCells: true,
-});
+	cellAlign: 'left',
+	contain: true,
+	groupCells: true
+})
+
+export default carousel
